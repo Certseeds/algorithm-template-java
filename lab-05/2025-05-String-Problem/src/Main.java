c95dd669565aca455f6e8109887f140ce0fea51c
Certseeds <51754303+Certseeds@users.noreply.github.com>
2025-11-16 11:24:36 +0000

    feat: 修复错误的 2025fall/lab05-String-Problem
    OJ上的测试用例不够, 会在一个简单用例上失败的代码仍然可以AC
    
    Signed-off-by: Certseeds <51754303+Certseeds@users.noreply.github.com>

--- lab-05/2025-05-String-Problem/src/Main.java
+++ lab-05/2025-05-String-Problem/src/Main.java
@@ -6,101 +6,87 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public final class Main {
+    private static final int ALPHABET = 26;
+    private static final int MIN_LENGTH = 1;
+    private static final int MAX_LENGTH = 500_000;
 
     private Main() {/**/}
 
-    private static final class InputData {
-        private final char[] cipher;
-        private final String target;
-
-        private InputData(char[] cipher, String target) {
-            this.cipher = cipher;
-            this.target = target;
-        }
-
-        private char[] cipher() {
-            return cipher;
-        }
-
-        private String target() {
-            return target;
-        }
+    public static void main(String[] args) throws IOException {
+        final var inputData = reader();
+        final var answer = cal(inputData);
+        output(answer);
     }
 
     public static InputData reader() throws IOException {
-        final var input = new Reader();
-        final char[] cipher = new char[26];
-        for (int i = 0; i < 26; i++) {
-            final String token = input.next();
+        final var fastReader = new Reader();
+        final var cipher = new char[ALPHABET];
+        for (int i = 0; i < ALPHABET; ++i) {
+            final var token = fastReader.next();
             assert (token != null);
-            assert (token.length() == 1);
+            assert ((token.length() == 1));
             final char mapped = token.charAt(0);
-            assert (('a' <= mapped) && (mapped <= 'z'));
+            assert ((mapped >= 'a') && (mapped <= 'z'));
             cipher[i] = mapped;
         }
-        final String target = input.next();
-        assert (target != null);
-        assert ((1 <= target.length()) && (target.length() <= 500000));
-        for (int i = 0; i < target.length(); i++) {
-            final char ch = target.charAt(i);
-            assert (('a' <= ch) && (ch <= 'z'));
+        final var text = fastReader.next();
+        assert (text != null);
+        final int length = text.length();
+        assert ((MIN_LENGTH <= length) && (length <= MAX_LENGTH));
+        for (int i = 0; i < length; ++i) {
+            final char ch = text.charAt(i);
+            assert ((ch >= 'a') && (ch <= 'z'));
         }
-        return new InputData(cipher, target);
+        return new InputData(cipher, text);
     }
 
     public static int cal(InputData data) {
-        final String s = data.target();
-        final int length = s.length();
-        final char[] mapped = new char[length];
-        final char[] cipher = data.cipher();
-        for (int i = 0; i < length; i++) {
-            mapped[i] = cipher[s.charAt(i) - 'a'];
+        final var s = data.text;
+        final int n = s.length();
+        final var mapped = new char[n];
+        for (int i = 0; i < n; ++i) {
+            mapped[i] = data.cipher[s.charAt(i) - 'a'];
         }
-        final char[] combined = buildCombined(s, mapped);
-        final int[] prefix = buildPrefixFunction(combined);
-        final int overlap = Math.min(prefix[combined.length - 1], length / 2);
-        // overlap is the visible portion of the second half that still matches
-        return length - overlap;
-    }
-
-    private static char[] buildCombined(String s, char[] mapped) {
-        final int length = s.length();
-        final char[] combined = new char[(2 * length) + 1];
-        for (int i = 0; i < length; i++) {
-            combined[i] = s.charAt(i);
-        }
-        combined[length] = '#';
-        for (int i = 0; i < length; i++) {
-            combined[length + 1 + i] = mapped[i];
-        }
-        return combined;
-    }
-
-    private static int[] buildPrefixFunction(char[] input) {
-        final int length = input.length;
-        final int[] prefix = new int[length];
-        for (int i = 1; i < length; i++) {
+        final var prefix = new int[n];
+        for (int i = 1; i < n; ++i) {
             int j = prefix[i - 1];
-            while ((j > 0) && (input[i] != input[j])) {
+            while ((j > 0) && (s.charAt(i) != s.charAt(j))) {
                 j = prefix[j - 1];
             }
-            if (input[i] == input[j]) {
-                j++;
+            if (s.charAt(i) == s.charAt(j)) {
+                ++j;
             }
             prefix[i] = j;
         }
-        return prefix;
-    }
-
-    public static void main(String[] args) throws IOException {
-        final InputData datas = reader();
-        final int result = cal(datas);
-        output(result);
+        int j = 0;
+        for (int i = 0; i < n; ++i) {
+            while ((j > 0) && (mapped[i] != s.charAt(j))) {
+                j = prefix[j - 1];
+            }
+            if (mapped[i] == s.charAt(j)) {
+                ++j;
+            }
+            if (j == n) {
+                j = prefix[j - 1];
+            }
+        }
+        final int maxPrefix = Math.min(j, n / 2);
+        return n - maxPrefix;
     }
 
     public static void output(int number) {
         System.out.print(number);
         System.out.print('\n');
+    }
+
+    private static final class InputData {
+        private final char[] cipher;
+        private final String text;
+
+        private InputData(char[] cipher, String text) {
+            this.cipher = cipher;
+            this.text = text;
+        }
     }
 
     // refactor from https://github.com/Kattis/kattio/blob/master/Kattio.java
@@ -117,13 +103,9 @@ public final class Main {
         String next() {
             while (st == null || !st.hasMoreElements()) {
                 try {
-                    final String line = br.readLine();
-                    if (line == null) {
-                        return null;
-                    }
-                    st = new StringTokenizer(line);
+                    st = new StringTokenizer(br.readLine());
                 } catch (IOException e) {
-                    throw new RuntimeException(e);
+                    e.printStackTrace();
                 }
             }
             return st.nextToken();
